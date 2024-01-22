a70bb26caa68e57b5226f93ff49f1ac20175e1b9
olusola joshua <joolaoluwa2002@gmail.com>
2024-01-22 23:44:06 +0100

    Created a new project, and added CORS

---
+++ Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Joshy_api.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder => 
+            builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+        });
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# This project is to model a version of an ecomm api
+# that deals with the sales of female related items
+# ranging from fashion to female wears and a lot of other interesting features.
+
+# Configured CORS to allow different domains to access the application by extending the IServiceCollection class.
+
+# More updates to come...
